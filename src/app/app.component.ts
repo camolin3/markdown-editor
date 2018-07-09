028f7dfe27ff90ec14785db655c10abb1e168156
Carlos Molina A <ing.cmolina@gmail.com>
2018-07-09 14:42:26 -0400

    Printing every operation in console. Configured the editor, added styles.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,12 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import 'brace/ext/language_tools.js';
+import 'brace/index';
+import 'brace/mode/markdown';
+import 'brace/theme/eclipse';
 import * as MarkdownIt from 'markdown-it';
 import * as emoji from 'markdown-it-emoji';
+import { AceEditorDirective } from 'ng2-ace-editor';
+import dedent from 'ts-dedent';
 import * as twemoji from 'twemoji';
 
 @Component({
@@ -8,15 +14,37 @@ import * as twemoji from 'twemoji';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  reqId = 0;
-  sId = 1;
-  currentLine = 0;
-  result = '';
-  quote = false;
+export class AppComponent implements AfterViewInit {
+  @ViewChild(AceEditorDirective) ace: AceEditorDirective;
+  result = dedent`# Hello!
 
+  This is an _online_ **Markdown** editor :v:.
+
+  Have some fun. Here you have a [Markdown cheatsheet](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet) ;).
+
+  `;
+  options = {
+    indentedSoftWrap: true,
+    printMargin: false,
+    wrapBehavioursEnabled: true,
+  };
 
   constructor() {
+  }
+
+  ngAfterViewInit() {
+    const editor = this.ace.editor;
+    const session = editor.getSession();
+    session.setUseWrapMode(true);
+    editor.renderer.setShowGutter(false);
+    editor.renderer.setPadding(20);
+    editor.renderer.setScrollMargin(20, 10);
+    editor.on('change', ({ action, start, lines }) => {
+      const pos = this.result.split('\n', start.row)
+        .map(l => l.length)
+        .reduce((acc, val) => acc + val, start.column + start.row);
+      console.log(action, JSON.stringify({ pos, lines }));
+    });
   }
 
   get resultString() {
